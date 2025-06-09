390e0262b21d07ad676881343d9d09c932ff1c79
jpechacek80 <jpechacek@yahoo.com>
2025-06-08 19:10:49 -0500

    TestOK with revised pages.js handler

--- pages.js
+++ pages.js
@@ -1,50 +1,58 @@
-//pages.js
-//html filename = route.html
-//
+import http from 'node:http';
+import path  from 'node:path';
 import * as fs from 'node:fs';
 
 const workdir= process.cwd();
-const htmlPath= workdir.concat(`/views`)
-const cssPath= workdir.concat(`/styles`)
-var status_code
 
-function loadpg(resp, url){
-  var filePath= get_filename(resp,url); 
-  console.log(`filePath= ${filePath}`)
+const fext = {
+  '.html': ['text/html', '/views'],
+  '.js':   ['text/javascript', '/js'],
+  '.css':  ['text/css', '/styles'],
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.wasm': 'application/wasm'
+};
 
-  if (!fs.existsSync(filePath)) { 
-    console.log(`Err:loadpg route= ${url}`)
-    resp.statusCode=404;
-    //resp.statusCode=301;
-    //resp.setHeader('Location','/about')
-    resp.end();
-    return; 
-  };
-  try{
-    resp.statusCode=200;
-    const data = fs.readFileSync(filePath, 'utf8');
-    resp.write(data);
-  } 
-  catch{
-    console.log(`Err:loadpg file ${filePath}`);
-    resp.statusCode=500;
+function loadpg (resp, url){
+  var webfile=url;
+  var extname= path.extname(url);
+  if(extname=='') {
+    extname='.html';
+    if (url == '/')  webfile = '/index.html';
+      else webfile =url.concat('.html')
   }
-    resp.end();
+  
+  const contentType = fext[extname][0] || 'application/octet-stream';
+var filepath;
+    filepath= workdir.concat(fext[extname][1]).concat(webfile); 
+ 
+    console.log(filepath)
+    console.log(contentType)
+  fs.readFile(filepath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile('./404.html', (err, content) => resp.end(content, 'utf-8'));
+      } else {
+        resp.writeHead(500);
+        resp.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      resp.writeHead(200, { 'Content-Type': contentType });
+      resp.end(content, 'utf-8');
+    }
+  });
 }
 
-function get_filename(resp, url){
-  var filepath;                //   /url  ('/' included)
-  console.log(`url= ${url}`)
-  if(url.includes ('.css')){
-    filepath= cssPath.concat(url); 
-    resp.setHeader('Content-Type', 'text/css');
-  }
-  else{
-    filepath= htmlPath.concat(url).concat('.html'); 
-    resp.setHeader('Content-Type', 'text/html');
-  }
-  return filepath;
-}
+
 
 export  {
   loadpg 
--- test.js
+++ test.js
@@ -1,14 +1,22 @@
 //test.js
-const sdfile = require('./sample_module');
-//const {greet} = require('./sample_module');   //selective import.
+const mimeTypes = {
+  '.html': ['text/html','views'],
+  '.js': ['text/javascript','js' ],
+  '.css': ['text/css','styles'],
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.wasm': 'application/wasm'
+};
 
-sdfile.greet(8);
-sdfile.meet('julian');
-var x= sdfile.add8(7);
-console.log(x);
-
-/**OUTPUT---
-hello,  8
-I'm julian
-15
- * */
+console.log ('ContentType=')
+console.log (mimeTypes['.html'][0])
+console.log (mimeTypes['.html'][1])
